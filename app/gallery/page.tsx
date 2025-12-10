0427c2b0b342cc7f8162f7d06f0da367d061fdc8
faty <145115890+fatoumatad223@users.noreply.github.com>
2025-12-10 23:46:52 +0100

    create: website pages

---
+++ app/gallery/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="text-7xl font-semibold tracking-tight text-black uppercase mb-10">
+            Les prestations
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
