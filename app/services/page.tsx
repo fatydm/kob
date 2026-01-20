2e190ba9afe9743f77219b6a7427dc646448cfe3
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-01-20 15:01:28 +0100

    create: prestations pages

---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Services(){
+    return(
+        <main className="min-h-screen flex items-center justify-center px-4 py-16 bg-gradient-to-br from-white to-[#f9f5f0]">
+            <section className="w-full max-w-3xl mt-20">
+                <div className="text-center mb-12">
+                    <h1>Nos prestations</h1>
+                    <p>Ici je mettrais les prestations</p>
+                </div>
+            </section>
+        </main>
+    )
+}
