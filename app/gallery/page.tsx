a6a0d873713b4a2b7f548e87b84931502be0284c
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-01-20 14:36:42 +0100

    update: gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,13 +1,11 @@
+import Gallery from "@/components/Gallery";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-between py-32 px-16">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-7xl font-semibold tracking-tight text-black uppercase mb-10">
-            Les prestations
-          </h1>
-        </div>
-      </main>
-    </div>
+    <main className="flex w-full flex-col items-center bg-[var(--firstback)]">
+      <div className="w-full max-w-6xl px-4">
+        <Gallery />
+      </div>
+    </main>
   );
 }
