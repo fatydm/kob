e39d63c42e28879b5019dbeb30ef327a89951eba
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-01-20 14:35:52 +0100

    create: contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+
+
+"use client";
+
+import ContactForm from "@/components/ContactForm";
+
+export default function Contact () {
+    return (
+        <>
+        <section>
+          <ContactForm />
+        </section>
+        </>
+    )
+}
