fd1452bece681417cd76a39a6b660f69f8b05ca9
faty <145115890+fatoumatad223@users.noreply.github.com>
2026-01-20 23:52:19 +0100

    edit: change modale photo

--- app/page.tsx
+++ app/page.tsx
@@ -28,17 +28,16 @@ export default function HomePage() {
   return (
     <>
       {showModal && (
-        <div
-          className="fixed inset-0 z-50 flex justify-center items-center
-                     bg-[url('https://i.pinimg.com/736x/93/6b/b9/936bb9a00c8b7e75b2698c6945ef7ad2.jpg')]
-                     bg-cover bg-center"
-        >
-          <div className="absolute inset-0 bg-black/40"></div>
+        <div className="
+            fixed inset-0 z-50 flex justify-center items-center
+            bg-[url('/assets/modale-img.png')]
+            bg-cover bg-center pb-30">
+          <div className="absolute inset-0 bg-black/60"></div>
 
           <div className="relative z-10 text-center">
             <button
               onClick={() => setShowModal(false)}
-              className="bg-white text-black px-6 py-3 rounded-lg hover:bg-gray-200 transition"
+              className="bg-[#8c8f82] text-black px-6 py-3 rounded-lg hover:bg-gray-200 transition"
             >
               Découvrez notre univers
             </button>
@@ -49,57 +48,47 @@ export default function HomePage() {
       <main className="flex w-full flex-col items-center">
         <div className="w-full max-w-6xl px-4">
           <section className="text-base font-medium m-10">
-          <Header />
-            <p className="text-black text-justify mb-5">
-              Bienvenue dans notre univers. Parce que chez K.O.B chaque femme mérite de se sentir sublime.
-              À travers le maquillage, je révèle votre éclat naturel et crée des looks élégants, modernes et intemporels.
+            <Header />
+            <p className="text-(--text-light) text-center text-2xl mb-5">
+              <span className="font-bold">Bienvenue dans notre univers.</span> <br/>
+              Parce que chez K.O.B chaque femme mérite de se sentir sublime.
+              À travers le maquillage, nous révélons votre éclat naturel et créons des looks élégants, modernes et intemporels.
             </p>
-            <p className="text-black text-justify mb-5">
-              Spécialisée dans le soft glam, le maquillage sophistiqué et les mises en beauté événementielles, 
-              je vous propose des prestations personnalisées, adaptées à votre personnalité, votre carnation et l'occasion.
-            </p>
-            <p className="text-black text-justify">
+            
+            <p className="text-(--text-light) text-center text-2xl mb-5">
               Que ce soit pour un moment important ou simplement pour apprendre à vous mettre en valeur, 
               chaque rendez-vous est pensé comme une véritable expérience beauté, alliant expertise, douceur et précision.
               <br />
               <br />
               Laissez-vous porter par une expérience beauté sur-mesure.
             </p>
-
-            <div className="flex flex-row items-center justify-center gap-5">
-              <Button
-                label="Notre galerie"
-                onClick={() => router.push("/gallery")}
-                type="button"
-                classes=""
-              />
-
-              <Button
-                label="Nos prestations"
-                onClick={() => router.push("/services")}
-                type="button"
-                classes="
-                  
-                  "
-              />
-
-              <Button
-                label="Nos formations"
-                onClick={() => router.push("/formations")}
-                type="button"
-                classes=""
-              />
-            </div>
           </section>
 
-          <section
-            className="mt-10 flex flex-col items-center mb-10 rounded-xl py-8"
-            style={{ backgroundColor: "var(--firstback)" }}
-          >
-            
-            <Gallery limit={6} clickable={false} /> 
+          <section className="flex flex-col items-center rounded-xl max-w-6xl">
+              <Gallery limit={6} clickable={false} /> 
+          </section>
 
-          
+          <section className="flex flex-row items-center justify-center gap-5 mb-15">
+            <Button
+              label="Notre galerie"
+              onClick={() => router.push("/gallery")}
+              type="button"
+              classes=""
+            />
+
+            <Button
+              label="Nos prestations"
+              onClick={() => router.push("/services")}
+              type="button"
+              classes=""
+              />
+
+            <Button
+              label="Nos formations"
+              onClick={() => router.push("/formations")}
+              type="button"
+              classes=""
+              />
           </section>
         </div>
       </main>
